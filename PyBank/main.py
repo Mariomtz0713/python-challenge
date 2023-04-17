d646f2e25dac6075a28fbf840e3ab7a06e7941e1
Mario Martinez <=>
2023-04-17 17:56:56 -0500

    45% done

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 budget_data = os.path.join('Resources', 'budget_data.csv')
 
-dates = []
+date = []
 net_amount = []
 monthly_change = []
 
@@ -20,31 +20,61 @@ def average_change(nums):
         total += x
     return total/size
 
+
 with open(budget_data, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
     
     for row in csvreader:
-        dates.append(row[0])
+        date.append(row[0])
         net_amount.append(int(row[1]))
 
 
-for x in range(len(dates)-1):
+#Creating monthly change list
+for x in range(len(date)-1):
     monthly_change.append(int(net_amount[x+1] - net_amount[x]))
+
+#Splitting Year/Month from date. Pulled from https://stackoverflow.com/questions/11714859/how-to-display-the-first-few-characters-of-a-string-in-python
+year = [date[:2] for date in date]
+month = [date[-3:] for date in date]
+
 greatest_increase = max(monthly_change)
+greatest_increase_index = monthly_change.index(greatest_increase)
+greatest_increase_year = year[greatest_increase_index + 1]
+greatest_increase_month = month[greatest_increase_index + 1]
+
 greatest_decrease = min(monthly_change)
+greatest_decrease_index = monthly_change.index(greatest_decrease)
+greatest_decrease_year = year[greatest_decrease_index + 1]
+greatest_decrease_month = month[greatest_decrease_index + 1]
 
 
 #print(type('net_amount'))
 
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months: {len(dates)}")
+print(f"Total Months: {len(date)}")
 print(f"Total: ${sum_amount(net_amount)}")
-print(f"Average Change: ${average_amount(monthly_change)}")
-print(f"Greatest Increase in profits: $({greatest_increase})")
-print(f"Greatest Decrease in Profits: $({greatest_decrease})")
+print(f"Average Change: ${round(average_change(monthly_change), 2)}")
+print(f"Greatest Increase in profits: {greatest_increase_month}-{greatest_increase_year} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {greatest_decrease_month}-{greatest_decrease_year} (${greatest_decrease})")
+
+#Writing Analysis
+output_path = os.path.join("analysis", "analysis.txt")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as txtfile:
+   
+   
+   
+   txtfile.write("Financial Analysis")
+   txtfile.write("----------------------------")
+   txtfile.write(f"Total Months: {len(date)}")
+   txtfile.write(f"Total: ${sum_amount(net_amount)}")
+   txtfile.write(f"Average Change: ${round(average_change(monthly_change), 2)}")
+   txtfile.write(f"Greatest Increase in profits: {greatest_increase_month}-{greatest_increase_year} (${greatest_increase})")
+   txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_month}-{greatest_decrease_year} (${greatest_decrease})")
 
 
-
+    
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python-challenge
+
+PyBank: 
+    Splitting Year/Month from full date pulled from
+     (https://stackoverflow.com/questions/11714859/how-to-display-the-first-few-characters-of-a-string-in-python)
+
+    Rounding format pulled from 
+    (https://www.programiz.com/python-programming/methods/built-in/round)
+
+PyPoll:
+    
